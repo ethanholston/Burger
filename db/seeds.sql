c02061b9bbd715d8d912b99a5ce944c456d621ad
Ethan <ethan.holston@gmail.com>
2018-11-03 17:06:38 -0400

    orm

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Cowboy", true), ("Diablo", true), ("Veggie", false);
---
+++ models/burger.js
@@ -0,0 +1,19 @@
+var orm = require("orm.js");
+
+var burger = {
+  selectAll = function(cb) {
+    orm.selectAll("burgers", function(res){
+      cb(res);
+    })
+  },
+  insertOne = function(cols, vals, cb){
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne = function(colVals, condition) {
+    orm.updateOne("burgers", colVals, condition, function(res) {
+      cb(res);
+    });
+  }
+}
